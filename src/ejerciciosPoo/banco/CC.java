0a2f3613bca56cc94cb8cfd0b6659506c5b75e64
Roma <145601442+Romavalles1998@users.noreply.github.com>
2024-02-13 13:31:21 +0100

    Add files via upload

---
+++ src/ejerciciosPoo/banco/CC.java
@@ -0,0 +1,52 @@
+/*package ejerciciosPoo.banco;
+
+import java.util.Set;
+
+public class CC {
+    private int nCC;
+    int cantidad;
+    private String nombre;
+    private Set<Cliente> clientes;
+
+    public CC(int n_cc, String nombre, Set<Cliente>) {
+        this.nCC = n_cc;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Set<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(Set<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+
+    public int getnCC() {
+        return nCC;
+    }
+
+    public void setnCC(int nCC) {
+        this.nCC = nCC;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void addCliente(Cliente cliente){
+        return this.clientes.add(cliente);
+    }
+}*/
---
+++ src/ejerciciosPoo/banco/Sucursal.java
@@ -0,0 +1,73 @@
+package ejerciciosPoo.banco;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*public class Sucursal {
+    private int n_suc;
+    private String direccion;
+    private Banco banco;
+    private Set<Prestamo> prestamos;
+    private CC CC;
+
+    public Sucursal(int n_suc, String direccion, Banco banco, CC CC) {
+        this.n_suc = n_suc;
+        this.direccion = direccion;
+        this.banco = banco;
+        this.prestamos = new HashSet<>();
+        this.CC = CC;
+    }
+
+    public CC getCuentaCorriente() {
+        return CC;
+    }
+
+    public CC setCuentaCorriente() {
+        return CC;
+    }
+
+    public int getN_suc() {
+        return n_suc;
+    }
+
+    public void setN_suc(int n_suc) {
+        this.n_suc = n_suc;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Banco getBanco() {
+        return banco;
+    }
+
+    public void setBanco(Banco banco) {
+        this.banco = banco;
+    }
+
+    public Set<Prestamo> getPrestamos() {
+        return prestamos;
+    }
+
+    public void setPrestamos(Set<Prestamo> prestamos) {
+        this.prestamos = prestamos;
+    }
+
+    public void addPrestamo(Prestamo prestamo) {
+        this.prestamos.add(prestamo);
+    }
+
+    @Override
+    public String toString() {
+        return this.n_suc + " - " + this.direccion;
+    }
+    public void addCC(CC cc){
+        this.CC.add(cc);
+    }
+}*/
+
